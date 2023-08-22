ef9311590f892e3c502fecb8acbf67781843cb7c
domuk-k <dannyworks102@gmail.com>
2023-08-23 01:14:58 +0900

    feat: music player - in the hall of mountain king
    - Grieg: Peer Gynt Suite No.1 "In the hall of the Mountain King

--- packages/jazzplayer/src/lib/jazzplayer.ts
+++ packages/jazzplayer/src/lib/jazzplayer.ts
@@ -1,7 +1,30 @@
-import { piano } from './piano';
+import * as Tone from 'tone';
+import { Midi, Track } from '@tonejs/midi';
 
-// const midiFilePath = './assets/peacepiece.mid';
+const midiFilePath = './assets/in-the-hall.mid';
 
-document.addEventListener('keyup', (e) => {
-  piano.triggerAttack('C3', 0, 1);
+const midi = await Midi.fromUrl(midiFilePath);
+const now = Tone.now() + 0.5;
+const synth = new Tone.Synth({
+  envelope: {
+    attack: 0.02,
+    decay: 0.1,
+    sustain: 0.3,
+    release: 1,
+  },
+}).toDestination();
+
+document.addEventListener('click', () => {
+  midi.tracks.forEach((track: Track) => {
+    track.notes.forEach((note) => {
+      synth.triggerAttackRelease(
+        note.name,
+        note.duration,
+        note.time + now,
+        note.velocity
+      );
+    });
+  });
+
+  Tone.Transport.start();
 });
